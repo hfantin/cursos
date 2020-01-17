5bdfa81bf92f564449b068a34daa0d5a629dc369
Hamilton Geraldo Fantin <hfantin@gmail.com>
2020-01-17 17:23:10 -0300

    capitulo 3 - inclui collection do postman e configura loja e fornecedor para usar o config-server

--- microservicos/fornecedor/build.gradle.kts
+++ microservicos/fornecedor/build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+	implementation("org.springframework.cloud:spring-cloud-starter-config")
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
