99e2a34aeff04734b853b18f58e0cbafc135a513
Hamilton Geraldo Fantin <hfantin@gmail.com>
2020-02-10 21:11:14 -0300

    cap 3 - view

---
+++ typescript/alurabank/app/ts/models/negociacoes.ts
@@ -0,0 +1,13 @@
+class Negociacoes {
+
+     // ou Negociacao[] == Array<Negociacao>
+    private _negociacoes: Array<Negociacao> = [];
+
+    adiciona(negociacao: Negociacao) {
+        this._negociacoes.push(negociacao);
+    }
+
+    paraArray(){
+        return this._negociacoes;
+    }
+}
