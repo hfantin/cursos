0652943f0a581e86356638d1c6a2480359f4539d
F3900699 Hamilton Geraldo Fantin <hamiltonfantin@bb.com.br>
2020-12-12 09:29:42 -0300

    ajustes

--- kotlin/src/colecoesparte3/Main.kt
+++ kotlin/src/colecoesparte3/Main.kt
@@ -4,6 +4,6 @@ fun main() {
     val assistiramCursoAndroid = listOf("Alex", "Fran", "Gui", "Maria")
     val assistiramCursoKotlin = listOf("Alex", "Paulo", "Maria")
     //    banco.nomes.add("Alex")
-    val assistiramAmbos = assistiramCursoKotlin ex+ assistiramCursoAndroid
+    val assistiramAmbos = assistiramCursoKotlin + assistiramCursoAndroid
     println(assistiramAmbos.distinct())
 }
