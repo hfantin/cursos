fc9b69c2c9fab4561b51916a54b2655b839988a0
F3900699 Hamilton Geraldo Fantin <hamiltonfantin@bb.com.br>
2021-02-04 08:05:50 -0300

    c13

--- rust/c13-function-features/workout/target/debug/deps/workout-d2d073994d8179d3.d
+++
@@ -1,5 +0,0 @@
-/opt/desenv/workspace/github/cursos/rust/c13-function-features/workout/target/debug/deps/workout-d2d073994d8179d3: src/main.rs
-
-/opt/desenv/workspace/github/cursos/rust/c13-function-features/workout/target/debug/deps/workout-d2d073994d8179d3.d: src/main.rs
-
-src/main.rs:
