19df6a3da991b1d763c41ca7c2e5a2c013109ddd
F3900699 Hamilton Geraldo Fantin <hamiltonfantin@bb.com.br>
2021-02-11 16:02:14 -0300

    c13

---
+++ rust/c13-function-features/iterators/src/main.rs
@@ -0,0 +1,90 @@
+fn main() {
+    println!("iterators");
+    let v1 = vec![1, 2, 3];
+    let iter = v1.iter();
+
+    // sum
+    // let total: i32 = iter.sum();
+    // println!("sum: {}", total)
+
+    // map
+    let v2: Vec<_> = iter.map(|x| x + 1).collect();
+    println!("{:?}", v2);
+
+    // for val in iter {
+    //     println!("value {}", val);
+    // }
+
+    // let mut iter = v1.iter();
+    // println!("{:?} ", iter.next());
+
+    // filter
+    let shoes = vec![
+        Shoe {
+            size: 10,
+            style: String::from("sneaker"),
+        },
+        Shoe {
+            size: 13,
+            style: String::from("sandal"),
+        },
+        Shoe {
+            size: 10,
+            style: String::from("boot"),
+        },
+    ];
+
+    let in_my_size = shoes_in_my_size(shoes, 10);
+
+    println!("shes in 10 size {:?}", in_my_size);
+
+    // implementing iterator
+    let mut counter = Counter::new();
+    println!("{:?}", counter.next());
+    println!("{:?}", counter.next());
+    println!("{:?}", counter.next());
+    println!("{:?}", counter.next());
+    println!("{:?}", counter.next());
+    println!("{:?}", counter.next());
+
+    let sum: u32 = Counter::new()
+        .zip(Counter::new().skip(1))
+        .map(|(a, b)| a * b)
+        .filter(|x| x % 3 == 0)
+        .sum();
+    println!("{}", sum);
+}
+
+#[derive(Debug)]
+struct Counter {
+    count: u32,
+}
+
+impl Counter {
+    fn new() -> Counter {
+        Counter { count: 0 }
+    }
+}
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.count < 5 {
+            self.count += 1;
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Shoe {
+    size: u32,
+    style: String,
+}
+
+fn shoes_in_my_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+    shoes.into_iter().filter(|s| s.size == shoe_size).collect()
+}
