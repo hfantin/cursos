d57ba51ed2cc719573638c336f25ba3e697bbe75
F3900699 Hamilton Geraldo Fantin <hamiltonfantin@bb.com.br>
2022-01-19 17:07:53 -0300

    cap 2

--- go/alura/api-rest/controllers/controllers.go
+++ go/alura/api-rest/controllers/controllers.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 
+	"github.com/gorilla/mux"
 	"github.com/hfantin/api-rest/models"
 )
 
@@ -14,4 +16,16 @@ func Home(w http.ResponseWriter, r *http.Request) {
 
 func TodasPersonalidades(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(models.Personalidades)
+}
+
+func UmaPersonalidade(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	for _, p := range models.Personalidades {
+		if strconv.Itoa(p.Id) == id {
+			json.NewEncoder(w).Encode(p)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(map[string]string{"error": "not found " + id})
 }
--- go/alura/api-rest/routes/routes.go
+++ go/alura/api-rest/routes/routes.go
@@ -4,12 +4,15 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/hfantin/api-rest/controllers"
 )
 
 func HandleFunc() {
-	http.HandleFunc("/", controllers.Home)
-	http.HandleFunc("/api/personalidades", controllers.TodasPersonalidades)
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	r := mux.NewRouter()
+	r.HandleFunc("/", controllers.Home)
+	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades).Methods("Get")
+	r.HandleFunc("/api/personalidades/{id}", controllers.UmaPersonalidade).Methods("Get")
+	log.Fatal(http.ListenAndServe(":8000", r))
 
 }
