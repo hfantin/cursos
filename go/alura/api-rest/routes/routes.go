560a1a2058fd2c679e5bb3ec2c23e596d4e6cce7
F3900699 Hamilton Geraldo Fantin <hamiltonfantin@bb.com.br>
2022-02-01 22:19:49 -0300

    cap 5

--- go/alura/api-rest/routes/routes.go
+++ go/alura/api-rest/routes/routes.go
@@ -4,18 +4,23 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/hfantin/api-rest/controllers"
+	"github.com/hfantin/api-rest/middleware"
 )
 
 func HandleFunc() {
 	r := mux.NewRouter()
-	r.HandleFunc("/", controllers.Home)
+	r.Use(middleware.ContentTypeMiddleware)
+	// r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalidades", controllers.ListarPersonalidades).Methods("Get")
 	r.HandleFunc("/api/personalidades/{id}", controllers.ObterPersonalidade).Methods("Get")
 	r.HandleFunc("/api/personalidades", controllers.CriarPersonalidade).Methods("Post")
 	r.HandleFunc("/api/personalidades/{id}", controllers.AtualizarPersonalidade).Methods("Put")
 	r.HandleFunc("/api/personalidades/{id}", controllers.ExcluirPersonalidade).Methods("Delete")
-	log.Fatal(http.ListenAndServe(":8000", r))
+	r.PathPrefix("/").Handler(http.FileServer(http.Dir("static/build")))
+	// log.Fatal(http.ListenAndServe(":8000", r))
+	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(r)))
 
 }
