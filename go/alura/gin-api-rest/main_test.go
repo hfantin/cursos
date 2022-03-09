ff0cfb5de29ab97ce4572a056a968f75c17e1a4b
F3900699 Hamilton Geraldo Fantin <hamiltonfantin@bb.com.br>
2022-03-09 13:49:51 -0300

    fim

--- go/alura/gin-api-rest/main_test.go
+++ go/alura/gin-api-rest/main_test.go
@@ -10,19 +10,60 @@ import (
 	"strconv"
 	"testing"
 
+	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/gin-gonic/gin"
 	"github.com/hfantin/cursos/gin-api-rest/controller"
 	"github.com/hfantin/cursos/gin-api-rest/database"
 	"github.com/hfantin/cursos/gin-api-rest/models"
 	"github.com/stretchr/testify/assert"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 var ID int
 
 func SetupDasRotasDeTeste() *gin.Engine {
-	gin.SetMode(gin.ReleaseMode)
+	gin.SetMode(gin.TestMode)
 	rotas := gin.Default()
 	return rotas
+}
+
+func openMockDB(t *testing.T) sqlmock.Sqlmock {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	// defer db.Close()
+	dialector := postgres.New(postgres.Config{
+		DSN:                  "sqlmock_db_0",
+		DriverName:           "postgres",
+		Conn:                 db,
+		PreferSimpleProtocol: true,
+	})
+	database.DB, err = gorm.Open(dialector, &gorm.Config{})
+	if err != nil {
+		t.Errorf("Failed to open gorm v2 db, got error: %v", err)
+	}
+
+	if database.DB == nil {
+		t.Error("gorm db is null")
+	}
+
+	// mock.ExpectBegin()
+	// mock.ExpectExec("UPDATE products").WillReturnResult(sqlmock.NewResult(1, 1))
+	// mock.ExpectExec("INSERT INTO product_viewers").WithArgs(2, 3).WillReturnResult(sqlmock.NewResult(1, 1))
+	// mock.ExpectCommit()
+
+	// postgres.Open(db.)
+	// gdb, err := gorm.Open("postgres", db)
+
+	// database.DB = gdb
+
+	//
+	// "SELECT * FROM "alunos" WHERE "alunos"."deleted_at" IS NULL" with expected regexp
+	// "SELECT * FROM "alunos" WHERE "alunos"."deleted_at" IS NULL"
+	return mock
+
 }
 
 func CriaAlunoMock() {
@@ -48,10 +89,11 @@ func TestVerificaStatusCodeDaSaudacaoComParametro(t *testing.T) {
 	assert.Equal(t, expected, string(body), "deveriam ser iguais")
 }
 
+// com mock do db
 func TestListaTodosOsAlunosHanlder(t *testing.T) {
-	database.Conectar()
-	CriaAlunoMock()
-	defer DeletaAlunoMock()
+	mock := openMockDB(t)
+	rows := sqlmock.NewRows([]string{"nome", "cpf", "rg"}).AddRow("jovem", "12345678900", "999888777")
+	mock.ExpectQuery(`^SELECT (.*) FROM "alunos" WHERE "alunos"."deleted_at" IS NULL$`).WillReturnRows(rows)
 	r := SetupDasRotasDeTeste()
 	r.GET("/alunos", controller.ListarAlunos)
 	req, _ := http.NewRequest("GET", "/alunos", nil)
