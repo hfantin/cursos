6676b64757a9c429bb15efc6d14c9e05fad8e383
F3900699 Hamilton Geraldo Fantin <hamiltonfantin@bb.com.br>
2022-04-08 08:57:54 -0300

    format

--- elixir/alura/math.exs
+++ elixir/alura/math.exs
@@ -1,7 +1,9 @@
 defmodule MeuModulo.Math do
-    def soma(p1, p2) do 
-        p1 + p2
-    end
-    def zero?(0), do: true 
-    def zero?(x) when(is_integer(x)), do: false
+  def soma(p1, p2) do
+    p1 + p2
+  end
+
+  def zero?(0), do: true
+  def zero?(x) when is_integer(x), do: false
+
 end
