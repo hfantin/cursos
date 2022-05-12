8bcf2bb518123919f413ae0deb93fdc022534ab8
F3900699 Hamilton Geraldo Fantin <hamiltonfantin@bb.com.br>
2022-05-12 18:30:21 -0300

    cap 6

--- elixir/programming_elixir_book/cap6_modules/exercises.exs
+++ elixir/programming_elixir_book/cap6_modules/exercises.exs
@@ -4,6 +4,16 @@ defmodule Exercises do
   def float_to_string(n) do
     :io.format("the number is ~.2f~n", n)
   end
+
   #  b. Get the value of an operating-system environment variable.
   def os(), do: System.get_env("HOME")
+
+  # Return the extension component of a file name (so return .exs if given "dave/test.exs").
+  def ext(file), do: Path.extname(file)
+
+  # Return the process’s current working directory.
+  def cwd(), do: File.cwd()
+
+  # Execute a command in your operating system’s shell.
+  def command(cmd), do: System.cmd(cmd, [])
 end
