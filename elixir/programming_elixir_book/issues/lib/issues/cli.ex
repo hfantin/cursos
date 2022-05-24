4ff24dc4ff1fa57aaf3a4d73585ef0785d6a6050
F3900699 Hamilton Geraldo Fantin <hamiltonfantin@bb.com.br>
2022-05-24 10:39:47 -0300

    issues project

--- elixir/programming_elixir_book/issues/lib/issues/cli.ex
+++ elixir/programming_elixir_book/issues/lib/issues/cli.ex
@@ -1,4 +1,6 @@
 defmodule Issues.CLI do
+  import Issues.TableFormatter, only: [print_table_for_columns: 2]
+
   @default_count 4
 
   @moduledoc """
@@ -19,13 +21,12 @@ defmodule Issues.CLI do
   Return a tuple of `{ user, project, count }`, or `:help` if help was given.
   """
   def parse_args(argv) do
-      OptionParser.parse(argv,
-        switches: [help: :boolean],
-        aliases: [h: :help]
-      )
-      |> elem(1)
-      |> args_to_internal_representation()
-
+    OptionParser.parse(argv,
+      switches: [help: :boolean],
+      aliases: [h: :help]
+    )
+    |> elem(1)
+    |> args_to_internal_representation()
   end
 
   def args_to_internal_representation([user, project, count]),
@@ -38,20 +39,37 @@ defmodule Issues.CLI do
   def args_to_internal_representation(_), do: :help
 
   def process(:help) do
-    IO.puts """
+    IO.puts("""
     usage: issues <user> <project> [ count | #{@default_count}]
-    """
+    """)
+
     System.halt(0)
   end
 
-  def process({user, project, _count}) do
+  def process({user, project, count}) do
     Issues.GithubIssues.fetch(user, project)
     |> decode_response()
+    |> sort_into_descending_order()
+    |> last(count)
+    |> print_table_for_columns(["numbers", "created_at", "title"])
   end
 
   def decode_response({:ok, body}), do: body
+
   def decode_response({:error, error}) do
-    IO.puts "Error fetching from Github: #{error["message"]}"
+    IO.puts("Error fetching from Github: #{error["message"]}")
     System.halt(2)
+  end
+
+  def sort_into_descending_order(list_of_issues) do
+    list_of_issues
+    # |> Enum.sort(fn i1, i2 -> i1["created_at"] >= i2["created_at"] end)
+    |> Enum.sort(&(&1["created_at"] >= &2["created_at"]))
+  end
+
+  def last(list, count) do
+    list
+    |> Enum.take(count)
+    |> Enum.reverse()
   end
 end
