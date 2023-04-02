85e55a8cec5e840b51b03377aac3da4663b0efb4
Hamilton Fantin <hfantin@gmail.com>
2023-04-02 14:07:48 -0300

    navigation cap 5

--- android/android-navigation/app/src/main/java/br/com/alura/aluraesporte/ui/fragment/DetalhesProdutoFragment.kt
+++ android/android-navigation/app/src/main/java/br/com/alura/aluraesporte/ui/fragment/DetalhesProdutoFragment.kt
@@ -14,8 +14,6 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.koin.core.parameter.parametersOf
 
 class DetalhesProdutoFragment : Fragment() {
-
-
     private val arguments by navArgs<DetalhesProdutoFragmentArgs>()
     private val produtoId by lazy { arguments.produtoId }
 
