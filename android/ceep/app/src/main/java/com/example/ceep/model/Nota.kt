0fa5845687973e3452189ac8fa4077383768ae32
Hamilton Geraldo Fantin <hfantin@gmail.com>
2023-06-11 12:23:18 -0300

    migrando projetos android de cursos da alura

--- android/ceep/app/src/main/java/com/example/ceep/model/Nota.kt
+++
@@ -1,7 +0,0 @@
-package com.example.ceep.model
-
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
-class Nota(val titulo: String, val descricao: String): Parcelable
--- android/room/app/src/main/java/br/com/alura/agenda/ui/activity/ConstantesActivities.java
+++
@@ -1,5 +0,0 @@
-package br.com.alura.agenda.ui.activity;
-
-interface ConstantesActivities {
-    String CHAVE_ALUNO = "aluno";
-}
